38b5ddf071115873ba3cd4a08b21a4420e98e81e
Farina007 <124944105+Farina007@users.noreply.github.com>
2023-11-24 15:53:26 -0300

    Add files via upload

---
+++ Projeto_Final/js/login.js
@@ -0,0 +1,16 @@
+document.querySelector("#login").addEventListener("click",()=>{
+fetch("http://192.168.1.60:3000/login",{
+method: "POST",
+headers: {
+'Content-type' : 'application/json'
+},
+body: JSON.stringify({
+"user" : document.querySelector("#user").value,
+"pass" : parseInt(document.querySelector("#pass").value)
+})
+}).then((res)=>{
+if(res.status == 200){
+window.location.href = "../html/index.html"
+}
+})
+})
